48cce1133198c9c5955b4210ea2be4302c99cec8
Kaleab Mulugeta <kaleabmulugeta@hotmail.com>
2024-07-18 17:49:07 +0300

    hot bug fix

--- wordOverlap.py
+++ wordOverlap.py
@@ -1,19 +1,17 @@
-import os
-from collections import Counter
-
-def read_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
+file_paths = ['./assets/amharic/cleantext.txt', './assets/tigrigna/cleantext.txt']
+def read_file(file):
+    with open(file, 'r', encoding='utf-8') as file:
         return file.read()
 
-def find_common_words(file_paths):
-    word_counts = Counter()
-    for file in file_paths:
-        text = read_file(file)
-        words = text.split()
-        word_counts.update(words)
-    common_words = [word for word, count in word_counts.items() if count == len(file_paths)]
-    return common_words
+def word_freq(file):
+    words = {}
+    data = read_file(file).split()
+    for word in data:
+        words[word] = words.get(word, 0) + 1
 
-file_paths = ['./assets/amharic/cleantext.txt', './assets/tigrigna/cleantext.txt']
-common_words = find_common_words(file_paths)
+def find_common_words():
+    st1 = set(read_file(file_paths[0]).split())
+    st2 = set(read_file(file_paths[1]).split())
+    return st1 & st2
+common_words = find_common_words()
 print(common_words)
